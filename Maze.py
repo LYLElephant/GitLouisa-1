c3930f3dbca7eb62d63115ca9346f1e61083443d
Louisa <3044673035@qq.com>
2020-03-28 08:20:52 +0800

    update 01

--- Maze.py
+++ Maze.py
@@ -1,1 +1,16 @@
 #This is an example
+import random
+from tkinter import *
+import tkinter.font as tkFont
+
+def set_Map(scale):
+    Map=[[0 for i in range(scale)] for j in range(scale)]
+    Map[1][scale-1]=2
+    Map[scale-2][0]=2 
+    for i in range(scale):
+        for j in range(scale):
+            if i%2==1 and j%2==1:
+                Map[i][j]=1
+    Map[scale-2][1]=2
+    return Map
+'''将生成一个原始“地图”的二维列表'''
